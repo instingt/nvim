100275e8d63845f778bf01953bbb94651f4325f2
Vitaliy Kataev <vita@kataev.pro>
2024-11-04 11:54:11 +0400

    Simplify Comment plugon config

--- lua/plugins/comments.lua
+++ lua/plugins/comments.lua
@@ -1,21 +1,17 @@
-local with_opts = require('utils').with_opts
-
 return {
-    'numToStr/Comment.nvim',
-    config = function()
-        local c = require("Comment")
-        c.setup()
-
-        vim.keymap.set("n", "<Leader>/", function()
-                return require("Comment.api").call(
-                    "toggle.linewise." .. (vim.v.count == 0 and "current" or "count_repeat"),
-                    "g@$"
-                )()
-            end,
-            with_opts("Toggle comment"))
-
-        vim.keymap.set("x", "<Leader>/",
-            "<Esc><Cmd>lua require('Comment.api').locked('toggle.linewise')(vim.fn.visualmode())<CR>",
-            with_opts("Toggle comment"))
-    end
+  "numToStr/Comment.nvim",
+  opts = {
+    toggler = {
+      line = "<Leader>/",
+      block = "<Leader>?",
+    },
+    opleader = {
+      line = "<Leader>/",
+      block = "<Leader>?",
+    },
+    mappings = {
+      basic = true,
+      extra = false,
+    },
+  },
 }
