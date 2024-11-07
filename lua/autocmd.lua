4290fef5779d968fe50e5cfa86dadb1909f0eb20
Vitaliy Kataev <vita@kataev.pro>
2024-11-07 16:50:13 +0400

    return noh on cursore move!

--- lua/autocmd.lua
+++ lua/autocmd.lua
@@ -28,3 +28,13 @@ autocmd("BufEnter", {
     end
   end,
 })
+
+autocmd("CursorMoved", {
+  group = vim.api.nvim_create_augroup("auto-hlsearch", { clear = true }),
+  desc = "disable search hightling on cursor move",
+  callback = function()
+    if vim.v.hlsearch == 1 and vim.fn.searchcount().exact_match == 0 then
+      vim.schedule(function() vim.cmd.nohlsearch() end)
+    end
+  end,
+})
