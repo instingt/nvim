a683ca25d2fa5823a34742c28d7b066423126bdc
Vitaliy Kataev <vita@kataev.pro>
2024-11-09 21:50:32 +0400

    feat: add toggleterm

---
+++ lua/plugins/toggleterm.lua
@@ -0,0 +1,16 @@
+return {
+  "akinsho/toggleterm.nvim",
+  version = "*",
+  config = function()
+    require("toggleterm").setup {
+      open_mapping = "<c-\\>",
+      direction = "float",
+      float_opts = {
+        border = "curved", -- 'single' | 'double' | 'shadow' | 'curved'
+        title_pos = "left", -- 'left' | 'center' | 'right'
+      },
+    }
+
+    vim.api.nvim_set_hl(0, "FloatBorder", { fg = "#ff79c6", bg = "#282a36" })
+  end,
+}
