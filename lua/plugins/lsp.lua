911fc57f0a9322e74e861b98c4a93ea453249a9d
Vitaliy Kataev <vita@kataev.pro>
2024-11-09 18:28:28 +0400

    feat: cmp and luasnip finally works fine

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -232,7 +232,44 @@ return {
         completion = { completeopt = "menu,menuone,noinsert" },
 
         mapping = cmp.mapping.preset.insert {
-          ["<cr>"] = cmp.mapping.confirm { select = true },
+          ["<CR>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              if luasnip.expandable() then
+                luasnip.expand()
+              else
+                cmp.confirm {
+                  select = true,
+                }
+              end
+            else
+              fallback()
+            end
+          end),
+          ["<tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+            elseif luasnip.locally_jumpable(1) then
+              luasnip.jump(1)
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+          ["<s-tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_prev_item()
+            elseif luasnip.locally_jumpable(-1) then
+              luasnip.jump(-1)
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+          ["<esc>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.close()
+            else
+              fallback()
+            end
+          end),
           ["<c-k>"] = cmp.mapping.select_prev_item(cmp_select),
           ["<c-j>"] = cmp.mapping.select_next_item(cmp_select),
           ["<c-b>"] = cmp.mapping.scroll_docs(-4),
