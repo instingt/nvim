7348215dade0e1721dd8606ecb6db578629b0695
Vitaliy Kataev <vita@kataev.pro>
2024-11-10 00:43:13 +0400

    style: improve theme

--- lua/plugins/color-scheme.lua
+++ lua/plugins/color-scheme.lua
@@ -2,5 +2,25 @@ return {
   "catppuccin/nvim",
   name = "catppuccin",
   priority = 1000,
-  config = function() vim.cmd.colorscheme "catppuccin-macchiato" end,
+  config = function()
+    require("catppuccin").setup {
+      flavour = "macchiato",
+      default_integrations = false,
+      integrations = {
+        cmp = true,
+        neotree = true,
+        gitsigns = true,
+        treesitter = true,
+        telescope = true,
+        navic = {
+          enabled = true,
+          custom_bg = "NONE",
+        },
+        harpoon = true,
+        which_key = true,
+      },
+    }
+
+    vim.cmd.colorscheme "catppuccin-macchiato"
+  end,
 }
