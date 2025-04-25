7a4434c94c0b6ed1b74a6639a5cb321a71eb5c9f
Vitaliy Kataev <vita@kataev.pro>
2025-04-25 23:32:08 +0400

    fix: disbale better escape inside terminals

--- lua/plugins/better-escape.lua
+++ lua/plugins/better-escape.lua
@@ -1,4 +1,11 @@
 return {
   "max397574/better-escape.nvim",
-  config = function() require("better_escape").setup() end,
+  event = "InsertEnter",
+  config = function()
+    require("better_escape").setup {
+      mappings = {
+        t = { j = { false } }, --lazygit navigation fix
+      },
+    }
+  end,
 }
